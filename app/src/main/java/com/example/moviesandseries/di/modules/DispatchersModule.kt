4491a4bb769675a2fe0d5a751d55e8a8d0be7c6c
tkintonis <thanos.kds@gmail.com>
2023-04-02 11:10:45 +0300

    reset

--- app/src/main/java/com/example/moviesandseries/di/modules/DispatchersModule.kt
+++ app/src/main/java/com/example/moviesandseries/di/modules/DispatchersModule.kt
@@ -3,10 +3,12 @@ package com.example.moviesandseries.di.modules
 import com.example.moviesandseries.common.helpers.DispatcherProvider
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DispatchersModule {
 
+    @Singleton
     @Provides
     fun providesDispatchersProvider() = DispatcherProvider()
 }
